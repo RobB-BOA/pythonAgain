e7ac5ba4e561d440637fe10d01db897304b410f6
RobB-BOA <66801961+RobB-BOA@users.noreply.github.com>
2021-02-18 14:24:38 -0700

    trying out branches

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import config
+import moreCode
 
 print('your password is ' + config.password)
+
+print(moreCode.var1 + ' ' + moreCode.var2)
---
+++ moreCode.py
@@ -0,0 +1,2 @@
+var1 = 'Stuff and things'
+var2 = 'More things and also stuff'
